a18271c50294f85e6a553e1677e66707cafe0959
vangheem <vangheem@gmail.com>
2020-06-09 22:52:15 -0400

    try with compose

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,10 +1,12 @@
+import kafkaesk
+import os
 import pytest
 import uuid
-import kafkaesk
 
 
 @pytest.fixture()
-async def app(kafka):
+async def app():
+    kafka = os.environ.get("KAFKA", "localhost:9092")
     yield kafkaesk.Application(
         [f"{kafka[0]}:{kafka[1]}"],
         topic_prefix=uuid.uuid4().hex,
