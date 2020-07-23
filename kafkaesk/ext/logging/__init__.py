66c7acc8395014bc47cae12567075879f8298b94
= <tyson@onna.com>
2020-07-23 13:44:37 -0400

    Fix flake8 issue after isort ran

--- kafkaesk/ext/logging/__init__.py
+++ kafkaesk/ext/logging/__init__.py
@@ -1,4 +1,4 @@
 from .formatter import PydanticFormatter  # noqa
-from .formatter import PydanticLogModel
+from .formatter import PydanticLogModel  # noqa
 from .handler import PydanticKafkaeskHandler  # noqa
-from .handler import PydanticStreamHandler
+from .handler import PydanticStreamHandler  # noqa
