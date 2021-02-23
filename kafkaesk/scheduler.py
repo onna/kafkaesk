31c83e80af035a05fab69a08a1c0f6a70e50e814
Carlos Garcia Marquez <carlos@lkxz.net>
2021-02-23 11:56:57 +0100

    WIP2

--- kafkaesk/scheduler.py
+++ kafkaesk/scheduler.py
@@ -1,20 +1,16 @@
 import asyncio
-import functools
 import queue
 from collections import defaultdict
 from typing import Optional
-
-
-class Record:
-    def __init__(self, offset, partition=None):
-        self.offset = offset
-        self.partition = partition
+from aiokafka import ConsumerRecord
+from aiokafka import TopicPartition
 
 
 class Job:
-    def __init__(self, coro, *, record: Record):
+    def __init__(self, coro, *, record: ConsumerRecord, tp: TopicPartition):
         self.coro = coro
         self.record = record
+        self.tp = tp
 
 
 class Scheduler:
@@ -27,39 +23,45 @@ class Scheduler:
     async def start(self):
         ...
 
-    async def spawn(self, coro, record: Record, timeout=None):
+    async def spawn(
+        self, coro, record: ConsumerRecord, tp: TopicPartition, timeout=None
+    ):
         await self._semaphore.acquire()
-        job = Job(coro=coro, record=record)
+        job = Job(coro=coro, record=record, tp=tp)
         fut = asyncio.create_task(job.coro)
-        self._running[job.record.partition].put((job.record.offset, job, fut))
+        self._running[tp].put((job.record.offset, fut))
         fut.add_done_callback(self.callback)
 
-    async def callback(self, future):
+    def callback(self, future):
         self._semaphore.release()
 
-    def get_offset(self) -> Optional[int]:
-        new_offset = dict()
-        for partition, running_queue in self._running.items():
-            while x := running_queue.get():
-                offset, job, fut = xn
-                if fut.done():
-                    new_offset[partition] = offset
-                else:
-                    running_queue.put(x)
-                    break
-        return new_offset
+    def get_offsets(self) -> Optional[int]:
+        # import pdb; pdb.set_trace()
+        new_offsets = dict()
+        for tp, running_queue in self._running.items():
+            try:
+                while x := running_queue.get_nowait():
+                    offset, fut = x
+                    if fut.done():
+                        new_offsets[tp] = offset
+                    else:
+                        running_queue.put(x)
+                        break
+            except queue.Empty:
+                continue
+        return new_offsets
 
     async def graceful_shutdown(self):
         futures = []
         for running_queue in self._running.values():
-            for _, _, fut in running_queue.queue:
+            for _, fut in running_queue.queue:
                 futures.append(fut)
         await asyncio.gather(*futures)
         await self.force_shutdown()
 
     async def force_shutdown(self):
         for running_queue in self._running.values():
-            for _, _, fut in running_queue.queue:
+            for _, fut in running_queue.queue:
                 fut.cancel()
                 try:
                     await fut
